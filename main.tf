427a12aec13086f2262cb9e6e4de2ff7167e98ea
SweetOps <vladimir.fess@gmail.com>
2017-08-29 00:12:28 +0300

    fixed variables of label module

--- main.tf
+++ main.tf
@@ -2,12 +2,14 @@ data "template_file" "default" {
   template = "${file("${var.ssh_public_key_path}")}"
 }
 
-
 module "label" {
-  source    = "git::https://github.com/cloudposse/tf_label.git?ref=tags/0.2.0"
-  namespace = "${var.namespace}"
-  stage     = "${var.stage}"
-  name      = "${var.name}"
+  source     = "git::https://github.com/cloudposse/tf_label.git?ref=tags/0.2.0"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
+  attributes = "${var.attributes}"
+  delimiter  = "${var.delimiter}"
+  tags       = "${var.tags}"
 }
 
 resource "aws_key_pair" "default" {
--- variables.tf
+++ variables.tf
@@ -10,6 +10,20 @@ variable "name" {
   default = ""
 }
 
+variable "delimiter" {
+  default = "-"
+}
+
+variable "attributes" {
+  type    = "list"
+  default = []
+}
+
+variable "tags" {
+  type    = "map"
+  default = {}
+}
+
 variable "ssh_public_key_path" {
   default = ""
 }
