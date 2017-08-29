aa18f036ef351363d119b1feb3c3019aada752d5
SweetOps <vladimir.fess@gmail.com>
2017-08-29 10:59:31 +0300

    fixed variables

--- variables.tf
+++ variables.tf
@@ -4,11 +4,19 @@ variable "stage" {}
 
 variable "name" {}
 
-variable "delimiter" {}
+variable "delimiter" {
+  default = "-"
+}
 
-variable "attributes" {}
+variable "attributes" {
+  type    = "list"
+  default = []
+}
 
-variable "tags" {}
+variable "tags" {
+  type    = "map"
+  default = {}
+}
 
 variable "ssh_public_key_path" {
   description = "Path to Read/Write SSH Public Key File (directory)"
