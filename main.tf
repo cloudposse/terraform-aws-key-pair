f158b61c9e2936f421f56d2072ba97bc59c7bacf
Jamie Nelson <jamie@theinstillery.com>
2018-05-19 14:58:10 +0100

    switched to terraform-terraform-label for modern sleekness

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.5"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.2"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
