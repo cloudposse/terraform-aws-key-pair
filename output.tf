3408ef26dd2ad5ed94ec83e68b5bfcff4529f12d
Vadim Hleif <vadzim.hleif@gmail.com>
2018-07-18 12:25:09 +0300

    Migrate readme yaml (#14)

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "key_name" {
-  value = "${element(compact(concat(aws_key_pair.imported.*.key_name, aws_key_pair.generated.*.key_name)), 0)}"
+  value       = "${element(compact(concat(aws_key_pair.imported.*.key_name, aws_key_pair.generated.*.key_name)), 0)}"
+  description = "Name of SSH key"
 }
 
 output "public_key" {
-  value = "${join("", tls_private_key.default.*.public_key_openssh)}"
+  value       = "${join("", tls_private_key.default.*.public_key_openssh)}"
+  description = "Contents of the generated public key"
 }
