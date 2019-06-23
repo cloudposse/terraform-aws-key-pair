383031b533397a4d77255719a9a1f7d7b12f931d
rverma-nikiai <rohit.verma@niki.ai>
2019-06-23 07:20:52 +0530

    pinned null label

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=master"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.13.0"
   namespace  = var.namespace
   stage      = var.stage
   name       = var.name
