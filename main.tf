174d46e66211ed14e13340d26b358b322afebaa9
Tim Gatzemeier <etwillbefine@users.noreply.github.com>
2019-09-14 21:13:12 +0200

    do not show private key content

--- main.tf
+++ main.tf
@@ -50,10 +50,10 @@ resource "local_file" "public_key_openssh" {
 }
 
 resource "local_file" "private_key_pem" {
-  count      = var.generate_ssh_key == true ? 1 : 0
-  depends_on = [tls_private_key.default]
-  content    = tls_private_key.default[0].private_key_pem
-  filename   = local.private_key_filename
+  count             = var.generate_ssh_key == true ? 1 : 0
+  depends_on        = [tls_private_key.default]
+  sensitive_content = tls_private_key.default[0].private_key_pem
+  filename          = local.private_key_filename
 }
 
 resource "null_resource" "chmod" {
