9d9c86fc4e339a4184ff1dea558070c1602a5761
luipugs <luipugs@users.noreply.github.com>
2020-02-15 14:31:20 +1100

    Fix spacing

--- main.tf
+++ main.tf
@@ -51,9 +51,9 @@ resource "local_file" "public_key_openssh" {
 }
 
 resource "local_file" "private_key_pem" {
-  count           = var.generate_ssh_key == true ? 1 : 0
-  depends_on      = [tls_private_key.default]
+  count             = var.generate_ssh_key == true ? 1 : 0
+  depends_on        = [tls_private_key.default]
   sensitive_content = tls_private_key.default[0].private_key_pem
-  filename        = local.private_key_filename
-  file_permission = "0600"
+  filename          = local.private_key_filename
+  file_permission   = "0600"
 }
