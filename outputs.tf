36ffb3e8bd42b775062ca282a4974862f893e406
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-07-02 19:48:25 -0700

    compatibility w/ `var.generate_ssh_key == false`

--- outputs.tf
+++ outputs.tf
@@ -22,12 +22,12 @@ output "public_key_filename" {
   description = "Public Key Filename"
 
   # Prevent releasing filename to downstream consumers until file exists (aka not during plan):
-  value = length(tls_private_key.default[0].public_key_openssh) > 0 ? local.public_key_filename : local.public_key_filename
+  value = length(join("", tls_private_key.default.*.public_key_openssh)) > 0 ? local.public_key_filename : local.public_key_filename
 }
 
 output "private_key_filename" {
   description = "Private Key Filename"
 
   # Prevent releasing filename to downstream consumers until file exists (aka not during plan):
-  value = length(tls_private_key.default[0].public_key_openssh) > 0 ? local.private_key_filename : local.private_key_filename
+  value = length(join("", tls_private_key.default.*.public_key_openssh)) > 0 ? local.private_key_filename : local.private_key_filename
 }
