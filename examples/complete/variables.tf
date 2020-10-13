ca6421ab4eb6cbde32f30d0c02f0d9c40da652e0
Frank Klaassen <f.klaassen@thesio.eu>
2020-10-13 13:42:50 +0200

    Convert to context

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -3,21 +3,6 @@ variable "region" {
   description = "AWS region"
 }
 
-variable "namespace" {
-  type        = string
-  description = "Namespace (e.g. `eg` or `cp`)"
-}
-
-variable "stage" {
-  type        = string
-  description = "Stage (e.g. `prod`, `dev`, `staging`)"
-}
-
-variable "name" {
-  type        = string
-  description = "Application or solution name (e.g. `app`)"
-}
-
 variable "ssh_public_key_path" {
   type        = string
   description = "Path to SSH public key directory (e.g. `/secrets`)"
--- main.tf
+++ main.tf
@@ -1,12 +1,6 @@
 module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.17.0"
-  namespace   = var.namespace
-  stage       = var.stage
-  environment = var.environment
-  name        = var.name
-  attributes  = var.attributes
-  delimiter   = var.delimiter
-  tags        = var.tags
+  source  = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
+  context = module.this.context
 }
 
 locals {
