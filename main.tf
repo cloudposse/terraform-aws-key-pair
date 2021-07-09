08c4c297857219c035dca4fb8d3410839b2799d3
Yonatan Koren <me@yonatankoren.com>
2021-07-09 14:03:37 -0400

    Ensure module adheres var.enabled; fix tests and ensure examples_import_key_test is run in test suite.

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 locals {
+  enabled = module.this.enabled
   public_key_filename = format(
     "%s/%s",
     var.ssh_public_key_path,
@@ -14,19 +15,19 @@ locals {
 }
 
 resource "aws_key_pair" "imported" {
-  count      = var.generate_ssh_key == false ? 1 : 0
+  count      = local.enabled && var.generate_ssh_key == false ? 1 : 0
   key_name   = module.this.id
   public_key = file(local.public_key_filename)
   tags       = module.this.tags
 }
 
 resource "tls_private_key" "default" {
-  count     = var.generate_ssh_key == true ? 1 : 0
+  count     = local.enabled && var.generate_ssh_key == true ? 1 : 0
   algorithm = var.ssh_key_algorithm
 }
 
 resource "aws_key_pair" "generated" {
-  count      = var.generate_ssh_key == true ? 1 : 0
+  count      = local.enabled && var.generate_ssh_key == true ? 1 : 0
   depends_on = [tls_private_key.default]
   key_name   = module.this.id
   public_key = tls_private_key.default[0].public_key_openssh
@@ -34,14 +35,14 @@ resource "aws_key_pair" "generated" {
 }
 
 resource "local_file" "public_key_openssh" {
-  count      = var.generate_ssh_key == true ? 1 : 0
+  count      = local.enabled && var.generate_ssh_key == true ? 1 : 0
   depends_on = [tls_private_key.default]
   content    = tls_private_key.default[0].public_key_openssh
   filename   = local.public_key_filename
 }
 
 resource "local_file" "private_key_pem" {
-  count             = var.generate_ssh_key == true ? 1 : 0
+  count             = local.enabled && var.generate_ssh_key == true ? 1 : 0
   depends_on        = [tls_private_key.default]
   sensitive_content = tls_private_key.default[0].private_key_pem
   filename          = local.private_key_filename
