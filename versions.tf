2c32780eabcf90e292babec6b9fe4c39befa34fc
nitrocode <nitrocode@users.noreply.github.com>
2021-08-28 14:33:55 -0400

    aws-3.0

--- versions.tf
+++ versions.tf
@@ -4,19 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = ">= 3.0"
     }
     tls = {
       source  = "hashicorp/tls"
       version = ">= 2.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.3"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.1"
     }
   }
 }
