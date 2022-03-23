e8f19cdb6973538eab71811b275286582f436e34
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2022-03-23 21:35:40 +0000

    Auto Format

--- main.tf
+++ main.tf
@@ -42,9 +42,9 @@ resource "local_file" "public_key_openssh" {
 }
 
 resource "local_sensitive_file" "private_key_pem" {
-  count             = local.enabled && var.generate_ssh_key == true ? 1 : 0
-  depends_on        = [tls_private_key.default]
-  content           = tls_private_key.default[0].private_key_pem
-  filename          = local.private_key_filename
-  file_permission   = "0600"
+  count           = local.enabled && var.generate_ssh_key == true ? 1 : 0
+  depends_on      = [tls_private_key.default]
+  content         = tls_private_key.default[0].private_key_pem
+  filename        = local.private_key_filename
+  file_permission = "0600"
 }
